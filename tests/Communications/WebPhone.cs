33b74966a017f3212c7d33ee23148abcb787fb79
Mitchel Constantin <mythicalbox@weavver.com>
2013-01-14 18:10:40 -0800

    renamed folders

---
+++ tests/Communications/WebPhone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using Weavver.Testing.App;
+
+namespace Weavver.Testing.Communication
+{
+     //[TestFixture]
+     public partial class WebPhone : WeavverTest
+     {
+//-------------------------------------------------------------------------------------------
+          [StagingTest]
+          public void Run()
+          {
+               WeavverApp weavver = new WeavverApp();
+
+               webDriver.Navigate().GoToUrl(BaseURL + "/about/");
+               WaitForPageLoad();
+
+               FindElement(By.LinkText("Web Phone")).Click();
+               FindElement(By.ClassName("ui-icon-closethick")).Click();
+          }
+//-------------------------------------------------------------------------------------------
+     }
+}
