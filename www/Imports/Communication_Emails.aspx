6c33aab45676b9da4cff91a5a271532772d6c5a6
Mitchel Constantin <mythicalbox@weavver.com>
2015-03-23 00:03:58 -0700

    experimenting with support for mailpipe's incoming e-mail api

---
+++ www/Imports/Communication_Emails.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Communication_Emails.aspx.cs" Inherits="Imports_Communication_Emails" ValidateRequest="false" %>
---
+++ www/Imports/Communication_Emails.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Weavver.Data;
+
+public partial class Imports_Communication_Emails : System.Web.UI.Page
+{
+     protected void Page_Load(object sender, EventArgs e)
+     {
+          if (Request.RequestType != "POST")
+          {
+               Response.Write("This page expects an HTTP POST");
+               return;
+          }
+
+          string eRecipient = Request.Form["recipient"];  
+          string eSender = Request.Form["sender"];
+          string sBodyStrippedPlain = Request.Form["stripped-text"]; // instead of body-plain
+          string eBodyStrippedHTML = Request.Form["stripped-html"]; // or body-html -- these have the signature/quoted parts remoted
+
+          using (Weavver.Data.WeavverEntityContainer data = new Weavver.Data.WeavverEntityContainer())
+          {
+               Communication_Emails incomingEmail = new Communication_Emails();
+               incomingEmail.Id = Guid.NewGuid();
+               incomingEmail.OrganizationId = new Guid(Request.QueryString["OrganizationId"]);
+               incomingEmail.From = eSender;
+               incomingEmail.To = eRecipient;
+               incomingEmail.HTMLBody = Request.Form["stripped-text"];
+               incomingEmail.Subject = Request.Form["subject"];
+               incomingEmail.CreatedAt = DateTime.UtcNow;
+               incomingEmail.CreatedBy = Guid.Empty;
+               incomingEmail.UpdatedAt = DateTime.UtcNow;
+               incomingEmail.UpdatedBy = Guid.Empty;
+               data.Communication_Emails.Add(incomingEmail);
+               data.SaveChanges();
+          }
+
+          Response.Write("OK");
+
+     }
+}
